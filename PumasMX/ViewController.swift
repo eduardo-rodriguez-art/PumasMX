515409ee992c653e423403f56142e78100db3c59
Jose Eduardo Rodriguez <joserodriguezr24@icloud.com>
2025-12-15 00:39:49 -0600

    Initial Commit

---
+++ PumasMX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PumasMX
+//
+//  Created by José Eduardo Rodríguez Reyes on 15/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
